f4dd148739ef23d91b8a1f77aa1f1235d7abe5d8
jacobsny <jacob.snyderman@gmail.com>
2019-09-21 06:44:45 -0400

    working on beginning

--- src/main.py
+++ src/main.py
@@ -39,12 +39,42 @@ def text_to_speech(text):
     return response
 
 
+def speech_to_text(path):
+    import io
+    import os
+
+    # Imports the Google Cloud client library
+    from google.cloud import speech
+    from google.cloud.speech import enums
+    from google.cloud.speech import types
+
+    # Instantiates a client
+    client = speech.SpeechClient()
+
+    # Loads the audio into memory
+    with io.open(path, 'rb') as audio_file:
+        content = audio_file.read()
+        audio = types.RecognitionAudio(content=content)
+
+    config = types.RecognitionConfig(
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=16000,
+        language_code='en-US')
+
+    # Detects speech in the audio file
+    response = client.recognize(config, audio)
+    responseString = ""
+    for result in response.results:
+        responseString += result.alternatives[0].transcript
+    return responseString
+
+
 def main():
     with open("src/que.txt") as f:
         lines = f.readlines()
     mainQ = lines.pop(0)
     text_to_speech(mainQ)
-
+    """trigger listen and speech to text"""
 
 
 
