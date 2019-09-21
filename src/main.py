8c515ce053bc7eba7284814738afc5862c4dbf00
jacobsny <jacob.snyderman@gmail.com>
2019-09-21 06:06:09 -0400

    working on beginning

--- src/main.py
+++ src/main.py
@@ -14,8 +14,37 @@ def detect_faces(path):
     return len(faces)
 
 
+def text_to_speech(text):
+    from google.cloud import texttospeech
+
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
+
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.types.SynthesisInput(text=text)
+
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.types.VoiceSelectionParams(
+        language_code='en-US',
+        ssml_gender=texttospeech.enums.SsmlVoiceGender.NEUTRAL)
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.types.AudioConfig(
+        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+    return response
+
+
 def main():
-    image_path = ""
-    while detect_faces(image_path) > 0:
+    with open("src/que.txt") as f:
+        lines = f.readlines()
+    mainQ = lines.pop(0)
+    text_to_speech(mainQ)
+
+
 
 
